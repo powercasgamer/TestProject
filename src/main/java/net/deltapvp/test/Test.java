89201fa2e98ca19b085ea22598865ddecd6a585a
powercas_gamer <cas@deltapvp.net>
2022-10-05 14:39:24 +0200

    release
    Signed-off-by: powercas_gamer <cas@deltapvp.net>

--- src/main/java/net/deltapvp/test/Test.java
+++ src/main/java/net/deltapvp/test/Test.java
@@ -27,6 +27,6 @@ package net.deltapvp.test;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 }
